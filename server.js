28f500e8d24fe9f9d8951db4e24039a1fe1d0508
Sergio Rodriguez <oneandprime@gmail.com>
2016-06-27 20:25:02 -0400

    test

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 var express = require("express"),
 	bodyParser = require("body-parser"),
+	router = express.Router(),
 	app = express(),
 	users = require('./routes/users'),
 	db = require("./db");
 
-
+var route = router.route("/")
+    .get(function(req,res){
+        var response = {};
+        db.users.find({},function(err,data){
+            if(err) {
+                response = {"error" : true,"message" : "Error fetching data"};
+            } else {
+                response = {"error" : false,"message" : data};
+            }
+            res.json(response);
+        });
+    });
+    
 routes();
 listen();
+
 
 
 function routes() {
 	app.use(bodyParser.json());
 	app.use(bodyParser.urlencoded({"extended" : false}));
-//	app.use('/users',users);
+	app.use('/',route);
 }
 
 function listen() {
